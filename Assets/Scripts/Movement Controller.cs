1a730d5735c724cb967cfa275da41b7a6bc4156a
williamkitto-cmyk <william.kitto@student.uts.edu.au>
2025-09-22 22:49:50 +1000

    Branch completed

---
+++ Assets/Scripts/Movement Controller.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    [Header("Tweener")]
+    [SerializeField] private Tweener tweener;
+
+    [Header("waypoints ")] [SerializeField]
+    private Transform[] waypoints;
+    
+    [Header("Animation")]
+    [SerializeField] private Animator animator;
+    
+    private float moveDuration = 1f;
+    
+    private int i = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = waypoints[0].position;
+        Debug.Log("pac man starting at "+transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!HasActiveTween())
+        {
+            MoveToNextWaypoint();
+        }
+    }
+
+    private void MoveToNextWaypoint()
+    {
+        int nextIndex = (i + 1) % waypoints.Length;
+        Vector3 startPos = transform.position;
+        Vector3 endPos = waypoints[nextIndex].position;
+        
+        AnimationDirection(endPos - startPos);
+
+        tweener.AddTween(transform, startPos, endPos, moveDuration);
+        i = nextIndex;
+    }
+    private bool HasActiveTween()
+    {
+       return tweener && tweener.HasActiveTween;
+    }
+
+    private void AnimationDirection(Vector3 direction)
+    {
+        Vector2 dir = new Vector2(direction.x, direction.y);
+        
+        dir.Normalize();
+
+        if (Mathf.Abs(dir.x) > Mathf.Abs(dir.y))
+        {
+            if (dir.x > 0)
+                animator.SetInteger("Direction", 1);
+            else
+                animator.SetInteger("Direction", 3);
+        }
+        else
+        {
+            if (dir.y > 0)
+                animator.SetInteger("Direction", 0);
+            else
+                animator.SetInteger("Direction", 2);
+        }
+    }
+}
---
+++ Assets/Scripts/Tweener.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tweener : MonoBehaviour
+{
+    private Tween activeTween;
+    public bool HasActiveTween => activeTween != null;
+
+    public void AddTween(Transform targetObject, Vector3 startPos, Vector3 endPos, float duration)
+    {
+        if (activeTween == null)
+        {
+            activeTween = new Tween(targetObject, startPos, endPos, Time.time,duration);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activeTween == null) return;
+
+        float elapsedTime = Time.time - activeTween.startTime;
+        float fraction = elapsedTime / activeTween.duration;
+        
+        activeTween.target.position = Vector3.Lerp(activeTween.startPos, activeTween.endPos, fraction);
+
+        if (fraction >= 1f)
+        {
+            activeTween.target.position = activeTween.endPos;
+            activeTween = null;
+        }
+    }
+}
